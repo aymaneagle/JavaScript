6ee9e8a869c40096c2b7a07762ec0845e322dca8
aymaneagle <aymanalwahid04@gmail.com>
2024-06-15 01:25:19 +0600

    "my own prototype"

--- miniG/Classes & Object/script.js
+++ miniG/Classes & Object/script.js
@@ -1,7 +1,29 @@
-const student = {
-    fullName: "ayman", 
-    mark: 61,
-    printMarks: function () {
-         console.log("marks = ", this.mark);
-    },
-};
+const employee = {
+   calcTax () {
+       console.log("tax rate is 10%");
+   }
+}
+
+//if object & prototype have same method, object method will be used
+const worker1 = {
+    salary : 40000,
+    calcTax () {
+        console.log("tax rate is 20%");
+    } 
+}
+
+const worker2 = {
+    salary : 55000,
+}
+
+const worker3 = {
+    salary : 60000,
+} 
+
+const worker4 = {
+    salary : 70000,
+}
+
+worker1.__proto__ = employee;
+worker2.__proto__ = employee;
+worker3.__proto__ = employee;
