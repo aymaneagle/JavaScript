9beab5e51e62ec4f234abe0d6d2a4cafab744965
aymaneagle <aymanalwahid04@gmail.com>
2024-06-19 23:14:32 +0600

    "basic class"

--- miniG/Classes & Object/script.js
+++ miniG/Classes & Object/script.js
@@ -1,29 +1,25 @@
-const employee = {
-   calcTax () {
-       console.log("tax rate is 10%");
-   }
+//basic class
+
+class toyotaCar {
+    start() {
+        console.log("start");
+    }
+
+    stop () {
+        console.log ("stop");
+    }
+
+    setBrand (brand) {
+        this.brandName = brand;
+    }
+    setSeries (series) {
+        this.series = series;
+    }
 }
 
-//if object & prototype have same method, object method will be used
-const worker1 = {
-    salary : 40000,
-    calcTax () {
-        console.log("tax rate is 20%");
-    } 
-}
 
-const worker2 = {
-    salary : 55000,
-}
-
-const worker3 = {
-    salary : 60000,
-} 
-
-const worker4 = {
-    salary : 70000,
-}
-
-worker1.__proto__ = employee;
-worker2.__proto__ = employee;
-worker3.__proto__ = employee;
+let fortuner = new toyotaCar ();
+fortuner.setBrand ("fortuner");
+fortuner.setSeries ("Joss");
+let lexus = new toyotaCar ();
+lexus.setSeries("beshi joss");
